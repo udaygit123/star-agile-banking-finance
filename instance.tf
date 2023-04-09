8c474e3b382da2dbca0d89c2921acfba7cc43969
unknown <shilimkarp123@gmail.com>
2023-04-09 21:20:27 +0530

    ansible playbbok and terraform chnages

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "production-server" {
- ami = "ami-02eb7a4783e7e9317"
+ ami = "ami-0a5dcff6fb7af3fc9"
  instance_type = "t2.micro"
  availability_zone = "ap-south-1"
  vpc_security_group_ids = ["sg-07d188f0228f6cce5"]
