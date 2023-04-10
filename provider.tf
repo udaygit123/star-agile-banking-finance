9f48a86b528dc2dbb09281e7cb25af1ee3a749dc
prafullashilimkar <122914577+prafullashilimkar@users.noreply.github.com>
2023-04-10 23:33:32 +0530

    key update

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
  region = "ap-south-1"
- access_key= "AKIAYOY47DPGY7NOQF3A"
- secret_key= "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"
+ access_key= "AKIAYOY47DPGXN2SAVLT"
+ secret_key= "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"
 }
