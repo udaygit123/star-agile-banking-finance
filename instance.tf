e19bc9fbe89993beb10b5084905fad92fcbaa219
prafullashilimkar <122914577+prafullashilimkar@users.noreply.github.com>
2023-04-10 22:38:02 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "production-server" {
- ami = "ami-02eb7a4783e7e9317"
+ ami = "ami-085d2183d3c2e3576"
  instance_type = "t2.micro"
- availability_zone = "ap-south-1"
+ availability_zone = ap-northeast-3""
  vpc_security_group_ids = ["sg-07d188f0228f6cce5"]
  key_name = "jenkinskey1"
  tags = {
