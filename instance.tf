3e730823543d02f0df4541f082fc8f97d62530ed
unknown <shilimkarp123@gmail.com>
2023-04-11 16:38:19 +0530

    update region

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "production-server" {
- ami = "ami-085d2183d3c2e3576"
+ ami = "ami-007855ac798b5175e"
  instance_type = "t2.micro"
- availability_zone = "ap-northeast-3"
+ availability_zone = "us-east-1a"
  vpc_security_group_ids = ["sg-07d188f0228f6cce5"]
  key_name = "jenkinskey1"
  tags = {
--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
 provider "aws" {
- region = "ap-northeast-3"
+ region = "provider "aws" {
+ region = "us-east-1"
  access_key= "AKIAYOY47DPGY7NOQF3A"
  secret_key= "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"
 }
