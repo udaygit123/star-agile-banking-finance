9a71c141336ae246fd057d94d173ce5c964ec0a3
unknown <shilimkarp123@gmail.com>
2023-04-12 14:51:16 +0530

    update

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws"{
  region = "us-east-1"
- access_key= "AKIAYOY47DPG44B4Q6N7"
- secret_key= "Q1lAFTlMPKGkWDv0lJqP7eGDpTJtg0jaRBRVmol"
+ access_key= "AKIAYOY47DPG2J3LFR5W"
+ secret_key= "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"
 }
