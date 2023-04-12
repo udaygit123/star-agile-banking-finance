09ac27d0cd04596975a0b5452f4d9d6ed154282b
prafullashilimkar <122914577+prafullashilimkar@users.noreply.github.com>
2023-04-12 23:29:51 +0530

    updated

--- test-server/provider.tf
+++ test-server/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
 region     = "ap-south-1" 
-access_key= "AKIAYOY47DPG2J3LFR5W"
-secret_key= "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"
+# access_key= "AKIAYOY47DPG2J3LFR5W"
+# secret_key= "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"
 }
