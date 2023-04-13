fcb09787202acd8190001b4e338889c7e8b95712
prafullashilimkar <122914577+prafullashilimkar@users.noreply.github.com>
2023-04-13 23:01:06 +0530

    Update provider.tf

--- test-server/provider.tf
+++ test-server/provider.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 provider "aws" {
-region     = "us-west-2" 
-access_key= "AKIAYOY47DPGQGNL3Q4D"
-secret_key= "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"
+region     = "us-east-1" 
 }
