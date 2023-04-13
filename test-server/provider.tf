77dc37cd0e7f83e5ab9e06998ed11e354c9b0e80
prafullashilimkar <122914577+prafullashilimkar@users.noreply.github.com>
2023-04-13 15:34:48 +0530

    region

--- test-server/provider.tf
+++ test-server/provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-region     = "ap-south-1" 
+region     = "us-west-2" 
 access_key= "AKIAYOY47DPGQGNL3Q4D"
 secret_key= "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"
 }
