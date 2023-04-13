5c7881e50d06cc62e02a9d5a07b08acc11946b9a
prafullashilimkar <122914577+prafullashilimkar@users.noreply.github.com>
2023-04-13 17:06:57 +0530

    subnet added

--- test-server/main.tf
+++ test-server/main.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "test-server" {
   ami           = "ami-0fcf52bcf5db7b003" 
   instance_type = "t2.micro" 
-  #key_name = "jenkinskey1"
-  #vpc_security_group_ids = ["sg-0ab5e99da98a87bfd"]
-  #security_groups = ["sg-0ab5e99da98a87bfd"]
+  key_name = "jenkinskey1"
+  subnet_id = "subnet-0d92bb9a83034e304"
+  vpc_security_group_ids = ["sg-0064e5f6f15377494"]
+ 
   connection {
     type     = "ssh"
     user     = "ubuntu"
