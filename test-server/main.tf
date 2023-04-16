4408cf767c24dd309c3405510477335676550df9
udaygit123 <122472075+udaygit123@users.noreply.github.com>
2023-04-16 21:13:45 +0530

    updated main file my project banking

--- test-server/main.tf
+++ test-server/main.tf
@@ -1,13 +1,13 @@
 resource "aws_instance" "test-server" {
-  ami           = "ami-02eb7a4783e7e9317" 
+  ami           = "ami-0aa2b7722dc1b5612" 
   instance_type = "t2.micro" 
-  key_name = "jenkinskey1"
-  vpc_security_group_ids = ["sg-0fab79b6d1427d7ef"]
+  key_name = "JenkinSerP1key"
+  vpc_security_group_ids = ["sg-077ce7b1c53c11b8a"]
  
   connection {
     type     = "ssh"
     user     = "ubuntu"
-    private_key = file("./jenkinskey1.pem")
+    private_key = file("./JenkinSerP1key.pem")
     host     = self.public_ip
   }
   provisioner "remote-exec" {
