c137078521f968ce862ac610c4c7916d65203a93
udaygit123 <122472075+udaygit123@users.noreply.github.com>
2023-04-17 23:30:47 +0530

    Update main.tf

--- test-server/main.tf
+++ test-server/main.tf
@@ -1,3 +1,20 @@
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 0
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_instance" "test-server1" {
   ami           = "ami-06e46074ae430fba6" 
   instance_type = "t2.micro" 
