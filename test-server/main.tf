8675ee58392fa6447be9112b7c9e6e03f8ac836d
udaygit123 <122472075+udaygit123@users.noreply.github.com>
2023-04-18 20:22:09 +0530

    Update main.tf

--- test-server/main.tf
+++ test-server/main.tf
@@ -21,6 +21,6 @@ resource "aws_instance" "test-server1" {
   }
    provisioner "local-exec" {
   #command = "ansiblePlaybook installation: 'ansible', inventory: '/etc/ansible/hosts', playbook: '/var/lib/jenkins/workspace/Banking-F-Project/test-server/myansible-playbook.yml'"
-   command = "ansible-playbook /var/lib/jenkins/workspace/Banking-F-Project/my-serverfiles/finance-playbook.yml"
+   command = "ansible-playbook /var/lib/jenkins/workspace/Banking-F-Project/my-server/myansible-playbook.yml"
    } 
 }
