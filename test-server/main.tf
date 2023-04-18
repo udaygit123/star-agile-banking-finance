080bfac90ac68f18ba5422311f045873c093b72b
udaygit123 <122472075+udaygit123@users.noreply.github.com>
2023-04-18 19:37:40 +0530

    updated

--- test-server/main.tf
+++ test-server/main.tf
@@ -20,6 +20,6 @@ resource "aws_instance" "test-server1" {
         command = " echo ${aws_instance.test-server1.public_ip} > inventory "
   }
    provisioner "local-exec" {
-  command = "ansiblePlaybook credentialsId: 'ubuntu', disableHostKeyChecking: true, installation: 'ansible', inventory: '/etc/ansible/hosts', playbook: '/var/lib/jenkins/workspace/Banking-F-Project/test-server/myansible-playbook.yml'"
+  command = "ansiblePlaybook disableHostKeyChecking: true, installation: 'ansible', inventory: '/etc/ansible/hosts', playbook: '/var/lib/jenkins/workspace/Banking-F-Project/test-server/myansible-playbook.yml'"
   } 
 }
